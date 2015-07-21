17a4023f76a6f2d56b6e063ae6a30b1f0fd53826
Fredy Gonzalo Captuayo Novoa <captuayonovoafredy@gmail.com>
2015-07-20 19:13:22 -0500

    Clases de apoyo

---
+++ rtuit/operations.py
@@ -0,0 +1,16 @@
+class operations:
+   numero_de_patas = 0
+   color = "marrón"
+
+   def calculateIdf():
+    	return 0
+	
+   def candidateWords11():
+    	return 
+
+   def betterWords(listWords):
+   		return listWords
+
+
+
+
